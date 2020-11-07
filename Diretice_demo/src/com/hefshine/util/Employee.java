52f814ed1deb183d42768678c362b54cdf3d739f
Ravikiran <hefshine.ravikiran@gmail.com>
2020-11-07 15:26:47 +0530

    directive

---
+++ Diretice_demo/src/com/hefshine/util/Employee.java
@@ -0,0 +1,18 @@
+package com.hefshine.util;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
+public class Employee {
+	
+	
+	
+	
+	public int Addition()
+	{
+		int c=10+20;
+		
+		return c;
+	}
+	
+
+}
